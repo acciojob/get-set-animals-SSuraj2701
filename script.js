aaad8eccf1db95b9cc99bf3d525fc65b04889522
Suraj Singh <suraj.job.2002@gmail.com>
2025-11-13 14:36:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Animal {}
+class Animal 
+{
+	Constructor(species){
+		this.species=species;
+	
+	}
+	get species(){
+		return this.species
+	}
+	makesound(){
+		return `The ${species} makes a sound`
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	Constructor(speices){
+	super(speices)
+}
+	bark(){
+		return `woof`
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	Constructor(species){
+		super(species)
+	}
+	return `purr`
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
